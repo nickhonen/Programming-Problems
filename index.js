53711b49556256e90590141adb191a5aefeb419f
Nicholas Honen <nickhonen@gmail.com>
2022-03-06 13:03:51 -0500

    attempted moveZeros didn't finish

---
+++ index.js
@@ -0,0 +1,19 @@
+
+
+function moveZeros(arr) {
+    const newArr = [];
+    // for each on array
+    // check if current value is zero, slice it
+    // then push a new 0 to end of array
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === 0) {
+        arr.splsomice(i, 1);
+        newArr.push(0);
+      }
+    }
+    console.log(newArr);
+    console.log(arr);
+    return arr;
+}
+
+console.log(moveZeros([ 9, 0, 9, 1, 2, 1, 1, 3, 1, 9, 0, 0, 9, 0, 0, 0, 0, 0, 0, 0 ]));
